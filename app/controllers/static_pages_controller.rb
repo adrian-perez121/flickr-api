75d1b63386fe86b525648c27c7355448174ea560
Adrian Perez <adrian1804perez@gmail.com>
2024-08-05 23:44:44 -0700

    Add a save feature for invalid user ids

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,11 @@ class StaticPagesController < ApplicationController
     @list = []
 
     unless params[:flickr_id].nil? || params[:flickr_id] == ''
-      @list = f.people.getPhotos(user_id: params[:flickr_id])
+      begin
+        @list = f.people.getPhotos(user_id: params[:flickr_id])
+      rescue
+        flash[:notice] = "Not a valid User ID"
+      end
     end
   end
 end
