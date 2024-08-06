9727d008ee01a48d3fba9a37d09d342ca1ff8d6c
Adrian Perez <adrian1804perez@gmail.com>
2024-08-05 23:37:47 -0700

    Add form to allow users to enter a user_id and see those images(only valid a valid user_id)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,10 @@ class StaticPagesController < ApplicationController
 
   def index
     f = Flickr.new
-    @list = f.people.getPhotos(user_id: '201187303@N04')
+    @list = []
+
+    unless params[:flickr_id].nil? || params[:flickr_id] == ''
+      @list = f.people.getPhotos(user_id: params[:flickr_id])
+    end
   end
 end
